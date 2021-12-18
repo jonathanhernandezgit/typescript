40ab8b7408cf34c2dbc0570e57455bf5cc86a67a
Jonathan Felipe Hernández Catalán <jfhernandez@ceroble.edu.gt>
2021-12-18 09:53:11 -0600

    Learning TypeScript: Functions

--- ts/index.ts
+++ ts/index.ts
@@ -65,4 +65,31 @@ comodin = {}
 
 
 
+//Funciones
+
+function add2(a: number, b: number): number {
+    return a + b;
+}
+
+const sum2 = add(4,6);
+
+function createAdder(a: number): (number) => number {
+    return function (b: number) {
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4)
+
+const fourPlus6 = addFour(6)
+
+function fullName(firstName: string, lastName?: string): string {
+    return `${firstName} ${lastName}`;
+}
+
+const jonathan = fullName('Jonathan','Hernandez');
+
+console.log(jonathan);
+
+
 
